d256800d1ec04e14937b1f9026727fd97897fe55
Mehmet Arda Demirci <110183411+MehmetArdaDemirci@users.noreply.github.com>
2022-08-11 19:20:20 +0300

    Add files via upload

---
+++ Uygulama.py
@@ -0,0 +1,4 @@
+taban = input("ucgenin taban degerini girin:")
+yukseklik = input("ucgenin yukseklik degerini girin:")
+ucgenin_alani = int(taban)*int(yukseklik)/2
+print("Taban:" + str(taban) + " Yukseklik:" + str(yukseklik) + " Ucgenin alani:" + str(ucgenin_alani))
