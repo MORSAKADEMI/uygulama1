c4a19e9466350ec331c7038ffa6c7fd8662fa091
Burak Üstün <79836033+burakbfm@users.noreply.github.com>
2022-08-11 18:47:18 +0300

    Create 1-Uygulama.py

---
+++ 1-Uygulama.py
@@ -0,0 +1,7 @@
+# Üçgenin alanı bulunurken, üçgenin taban uzunluğu ile yüksekliğinin çarpımının 2'ye bölünmesi ile üçgenin alanı bulunmuş olmaktadır
+
+# Kullanıcıdan üçgenin alanını al
+# İşlemleri yap ve alanı bul yazdır
+# Karşıya cevap "Uzunluk: 5 , Yükseklik: 6 - Üçgenin Alanı: "
+
+print("Burak")
