8acdaa7cb0f34220fd9a1e4f59b80209da9240a5
yigit000 <alpygt.2007@gmail.com>
2022-08-11 19:46:18 +0300

    ucgeninalani
    yigit

---
+++ ucgeninalaniduzenlendi-yigit.py
@@ -0,0 +1,4 @@
+x = input("ucgenin taban uzunlugu: ")
+y = input("ucgen yukseklik: ")
+sonuc = int(x)*int(y)/2
+print("ucgenin taban uzunlugu:" + str(x) + "\t" + "yukseklik:" + str(y) + " " + "\t" + "sonuc:" + str(sonuc))
